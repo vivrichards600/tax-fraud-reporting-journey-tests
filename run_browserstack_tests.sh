0096c32b9837d7b0d1ce06b2c1c265099d10e5f1
ryanniven <25480115+ryanniven@users.noreply.github.com>
2022-03-10 12:13:05 +0000

    BROWSERSTACK: got 2 browsers on mac working

--- run_browserstack_tests.sh
+++ run_browserstack_tests.sh
@@ -19,16 +19,14 @@ sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${usernam
 #sbt -Dbrowser=browserstack -Dlanguage=english -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser_version="11" -Dbrowserstack.browser="IE" -Dbrowserstack.os="Windows" -Dbrowserstack.os_version="10" "testOnly uk.gov.hmrc.test.ui.specs.*"
 
 #mac
-##needs to be reviewed manually
-#sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser="Safari" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Mojave" "testOnly uk.gov.hmrc.test.ui.specs.*"
-#sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser_version="latest" -Dbrowserstack.browser="Chrome" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Monterey" "testOnly uk.gov.hmrc.test.ui.specs.*"
-#sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser_version="latest" -Dbrowserstack.browser="Firefox" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Monterey" "testOnly uk.gov.hmrc.test.ui.specs.*"
+#needs to be reviewed manually
+#sbt -Dbrowser=browserstack -Dlanguage=english -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.local="true" -Dbrowserstack.browser="Safari" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Catalina" 'testOnly uk.gov.hmrc.test.ui.specs.*'
+sbt -Dbrowser=browserstack -Dlanguage=english -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser_version="latest" -Dbrowserstack.browser="Firefox" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Catalina" 'testOnly uk.gov.hmrc.test.ui.specs.*'
+sbt -Dbrowser=browserstack -Dlanguage=english -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.browser_version="latest" -Dbrowserstack.browser="Chrome" -Dbrowserstack.os="OS X" -Dbrowserstack.os_version="Catalina" 'testOnly uk.gov.hmrc.test.ui.specs.*'
 
 #iOS
 ##needs to be reviewed manually
-#sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.real_mobile="true" -Dbrowserstack.device="iPhone 12" -Dbrowserstack.os_version="14" -Dbrowserstack.local=true "testOnly uk.gov.hmrc.test.ui.specs.*"
+sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey} -Dbrowserstack.real_mobile="true" -Dbrowserstack.device="iPhone 12" -Dbrowserstack.os_version="14" -Dbrowserstack.local=true "testOnly uk.gov.hmrc.test.ui.specs.*"
 
 #Android
 sbt -Dbrowser=browserstack -Denvironment=local -Dbrowserstack.username=${username} -Dbrowserstack.key=${automatekey}  -Dbrowserstack.real_mobile="true" -Dbrowserstack.device="Samsung Galaxy S20" -Dbrowserstack.os_version="10.0" -Dbrowserstack.browser="samsung" "testOnly uk.gov.hmrc.test.ui.specs.*"
-
-
