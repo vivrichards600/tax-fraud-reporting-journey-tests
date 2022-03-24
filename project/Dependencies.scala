ee8afefa16a59d61161aa5e12470364c5f854ed4
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2022-03-24 14:44:19 +0000

    PLATUI-1638 - updating test repo to use selenium 4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "webdriver-factory"  % "0.25.0"  % Test,
-    "org.scalatest"       %% "scalatest"          % "3.2.0"   % Test,
-    "org.scalatestplus"   %% "selenium-3-141"     % "3.2.0.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all"       % "0.35.10" % Test,
-    "org.pegdown"          % "pegdown"            % "1.2.1"   % Test,
-    "com.typesafe"         % "config"             % "1.3.2"   % Test,
-    "org.mongodb.scala"   %% "mongo-scala-driver" % "4.4.0",
-    "uk.gov.hmrc"         %% "domain"             % "7.0.0-play-28"
+    "uk.gov.hmrc"         %% "webdriver-factory-upgrade-spike" % "0.3.0"    % Test,
+    "org.scalatest"       %% "scalatest"                       % "3.2.11"   % Test,
+    "org.scalatestplus"   %% "selenium-4-1"                    % "3.2.11.0" % Test,
+    "com.vladsch.flexmark" % "flexmark-all"                    % "0.62.2"   % Test,
+    "org.pegdown"          % "pegdown"                         % "1.2.1"    % Test,
+    "com.typesafe"         % "config"                          % "1.3.2"    % Test,
+    "org.mongodb.scala"   %% "mongo-scala-driver"              % "4.4.0",
+    "uk.gov.hmrc"         %% "domain"                          % "7.0.0-play-28"
   )
 
 }
